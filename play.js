99cff2be18a77088b034dffaa654d7ccc6bc5ca1
Al Lotina <lotina.al@gmail.com>
2020-09-23 21:01:25 +0000

    play.js modified to include stdin

--- play.js
+++ play.js
@@ -1,5 +1,25 @@
 // const net = require('net');
 // object { connect } instead  of variable ?
 const connect = require('./client');
-console.log('Connecting ...');
+// console.log('Connecting ...');
 connect();
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  const handleUserInput = stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+
+  return stdin;
+}
+
+setupInput();
