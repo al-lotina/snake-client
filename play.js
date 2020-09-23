3609b0cb7642e231ee97e320e9b3bed3bc9c546c
Al Lotina <lotina.al@gmail.com>
2020-09-23 21:12:27 +0000

    input.js added to contain input functions from play.js

--- play.js
+++ play.js
@@ -3,23 +3,7 @@
 const connect = require('./client');
 // console.log('Connecting ...');
 connect();
-
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  const handleUserInput = stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-
-  return stdin;
-}
+// object { setupInput } instead  of variable ?
+const setupInput = require('./input');
 
 setupInput();
